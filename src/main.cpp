dabe732f2f99d939b7313049fdd920575544d8bd
Richard Swinkels <github@richardswinkels.nl>
2025-02-14 20:54:14 +0100

    Init lcd and draw player sprite

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,41 @@
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <SPI.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define TFT_CS    D8
+#define TFT_RST   D3
+#define TFT_DC    D4
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+GFXcanvas16 canvas(128, 160);
+
+const unsigned char playerSprite [] PROGMEM = {
+	0x30, 0x30, 0x78, 0x78, 0xfc, 0xfc, 0xcc
+};
+
+unsigned int playerX = 58;
+unsigned int playerY = 145;
+
+void setupScreen()
+{
+  tft.initR(INITR_18BLACKTAB);
+  tft.fillScreen(ST7735_BLACK);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void drawGameScreen()
+{
+  canvas.fillScreen(ST7735_BLACK);
+  canvas.drawBitmap(playerX, playerY, playerSprite, 6, 7, ST7735_WHITE);
+  tft.drawRGBBitmap(0, 0, canvas.getBuffer(), 128, 160);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void setup()
+{  
+  setupScreen();
+}
+
+void loop()
+{
+  drawGameScreen();
 }
